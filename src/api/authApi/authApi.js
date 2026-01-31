85ccb720b3f47847dfbc677a0c05fbaa195de46e
Номи Шумо <email@.com>
2026-01-31 09:53:45 +0500

    soz shud

--- src/api/authApi/authApi.js
+++ src/api/authApi/authApi.js
@@ -1,26 +1,16 @@
 import { createAsyncThunk } from "@reduxjs/toolkit";
-import { message } from "antd";
-import { apiInstance } from "../../utils/url";
+import axios from "axios";
+import { apiInstance, axiosRequest } from "../../utils/url";
 
-export const loginUser = createAsyncThunk(
-  "auth/loginUser",
-  async (user, { rejectWithValue }) => {
-    try {
-      const { data } = await apiInstance.post("/Account/login", user);
 
-      const hour = new Date().getHours();
-      const isWorkTime = hour >= 9 && hour < 18;
 
-      if (!isWorkTime && data.role !== "admin") {
-        message.error("Working time is over");
-        return rejectWithValue("TIME_EXPIRED");
-      }
-
-      message.success("Login successful");
-      return data;
-    } catch (error) {
-      message.error("Login failed");
-      return rejectWithValue(error.response?.data || "ERROR");
-    }
+export const loginUser = createAsyncThunk("auth/loginUser", async (user) => {
+  try {
+    let { data } = await apiInstance.post(`/Account/login`, user);
+     
+    return data;
+    
+  } catch (error) {
+    console.log(error);
   }
-);
+});
